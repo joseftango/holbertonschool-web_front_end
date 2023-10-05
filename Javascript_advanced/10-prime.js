a6871b573103cf49bcd78aeb5c9e08d349d8114d
joseftango <4279@holbertonschool.com>
2023-10-05 18:30:38 +0100

    10-prime.js

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,42 @@
+function isPrime(number) {
+	if (number <= 1) return false;
+	if (number <= 3) return true;
+
+	if (number % 2 === 0 || number % 3 === 0) return false;
+
+	let i = 5;
+
+	while (i * i <= number) {
+	  if (number % i === 0 || number % (i + 2) === 0) return false;
+	  i += 6;
+	}
+
+	return true;
+}
+  
+function countPrimeNumbers() {
+	const startTime = performance.now();
+	let count = 0;
+
+	for (let i = 2; i <= 100; i++) {
+	  if (isPrime(i)) {
+		count++;
+	  }
+	}
+
+	const endTime = performance.now();
+	const executionTime = endTime - startTime;
+
+	console.log(`Execution time of counting prime numbers was ${executionTime} milliseconds.`);
+
+	return count;
+}
+
+const start = performance.now();
+
+for (let i = 0; i < 100; i++) {
+	countPrimeNumbers()
+}
+
+const end = performance.now();
+console.log(`Execution time of counting prime numbers was ${end - start} milliseconds.`);
