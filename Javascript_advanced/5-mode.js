b850ebc2b130f65e0385b940bac2903215675f84
joseftango <4279@holbertonschool.com>
2023-10-05 12:41:45 +0100

    5-mode.js

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,9 +1,11 @@
 function changeMode(size, weight, transform, background, color) {
-	document.documentElement.style['font-size'] = size;
-	document.documentElement.style['font-weight'] = weight;
-	document.documentElement.style['text-transform'] = transform;
-	document.documentElement.style['background-color'] = background;
-	document.documentElement.style['color'] = color;
+	return function() {
+		document.documentElement.style['font-size'] = size;
+		document.documentElement.style['font-weight'] = weight;
+		document.documentElement.style['text-transform'] = transform;
+		document.documentElement.style['background-color'] = background;
+		document.documentElement.style['color'] = color;	
+	}
 }
 
 function main() {
@@ -15,25 +17,20 @@ function main() {
 	spooky.textContent = 'Spooky';
 	document.body.appendChild(spooky);
 
-	spooky.onclick = function() {
-		changeMode(9, 'bold', 'uppercase', 'pink', 'green');
-	}
+	spooky.onclick = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+	
 
 	const darkMode = document.createElement('button');
 	darkMode.textContent = 'Dark mode';
 	document.body.appendChild(darkMode);
 
-	darkMode.onclick = function() {
-		changeMode(12, 'bold', 'capitalize', 'black', 'white');
-	}
+	darkMode.onclick = changeMode(12, 'bold', 'capitalize', 'black', 'white');
 
 	const screamMode = document.createElement('button');
 	screamMode.textContent = 'Scream mode';
 	document.body.appendChild(screamMode);
 
-	screamMode.onclick = function() {
-		changeMode(12, 'normal', 'lowercase', 'white', 'black');
-	}
+	screamMode.onclick = changeMode(12, 'normal', 'lowercase', 'white', 'black');
 }
 
 main()
